f495a7fbf387c0913c367f56d4e10024adb83338
Eric L. Anderson <ericlanderson@gmail.com>
2020-05-26 12:51:29 -0500

    Updates: moved common turn code to function; move Game class stuff

--- koans/greed.py
+++ koans/greed.py
@@ -15,7 +15,11 @@ import random
 class Game:
     def __init__(self,names):
         self._players = []
+        self.round = 1
+        self.over = False
         self._message = ""
+        self.final_round = False
+        self.leader = Player()
         for name in names:
             print("Adding ",name, " to the game.")
             self._players.append(Player(name))
@@ -23,21 +27,41 @@ class Game:
     @property
     def players(self):
         return self._players
-
-    @property
-    def leader(self):
-        leader = ""
+    
+    def get_leader(self):
+        current_leader = Player()
         highest_score = 0
         for player in self._players:
             if player.score > highest_score:
-                leader = player.name
-                highest_score = player.score
-        return Player(leader, highest_score)
+                current_leader = player
+                highest_score = current_leader.score
+        return current_leader
 
     def remove_player(self,leader):
         for player in self._players:
             if player.name == leader.name:
                 self._players.remove(player)
+
+    def next_round(self):
+        if self.final_round: 
+            # Game over
+            self.over = True
+            final_round_leader = self.get_leader()
+            if final_round_leader.score > self.leader.score:
+                self.leader = final_round_leader
+        else:
+            # End of current round
+            self.leader = self.get_leader()
+            if self.leader.name == "None":
+                print("End of round",self.round,": There is no leader.")
+            else:
+                print("End of round",self.round,":",self.leader.name,"is the leader with",self.leader.score,"points.")
+
+            self.round += 1
+
+    def end(self):
+        print("******************************************************")
+        print("After",self.round,"rounds,",self.leader.name,"is the winner with",self.leader.score,"points.")
 
     @property
     def message(self):
@@ -48,21 +72,39 @@ class Game:
         self._message = msg
 
 class Player:
-    def __init__(self,name, score = 0):
+    def __init__(self,name = "None", score = 0):
         self._name = name
         self._score = score
+        self._message = ""
+        self._scratched = False
  
     @property
     def name(self):
         return self._name
 
-    @property
+    @property 
     def score(self):
         return self._score
+
+    @property
+    def scratched(self):
+        return self._scratched
+
+    @scratched.setter
+    def scratched(self, value):
+        self._scratched = value
 
     @score.setter
     def score(self, value):
         self._score = value
+
+    @property
+    def message(self):
+        return self._message
+    
+    @message.setter
+    def message(self,msg):
+        self._message = msg
 
 class DiceSet:
     def __init__(self):
@@ -125,67 +167,71 @@ class DiceSet:
         self._score = score
         self._remaining = remaining
 
+def take_turn(player,remaining_dice,turn_score):
+    dice = DiceSet()
+    dice.roll(remaining_dice)
+    print(player.name,"rolled",dice.values,"which is",dice.score,"points.")
+    if dice.score > 0:
+        turn_score += dice.score
+        if dice.remaining > 0:
+            remaining_dice = dice.remaining
+        else:
+            remaining_dice = 5
+    else:
+        player.scratched=True
+    
+    return player, remaining_dice, turn_score
+
 max_score = 3000
 
 players = ["Andy","Candy","Mandy","Sandy","Glenn Dandy"]
 game = Game(players)
 
-leader = Player("Anon",0)
-
-game.rounds = 0
-game.over = False
-final_round = False
 while not game.over: 
 
-    game.rounds += 1
-
-    if leader.score >= max_score:
-        final_round = True
-        game.remove_player(leader)
+    if game.leader.score >= max_score:
+        game.final_round = True
+        game.remove_player(game.leader)
         game.message = "(final round)"
 
     print("******************************************************")
-    print("Round:",game.rounds,game.message)
+    print("Round:",game.round,game.message)
 
     for player in game.players:
-        print(player.name, "is taking a turn and currently has",player.score,"points.")
         
         remaining_dice = 5
         turn_score = 0
         scratched = False
+        player.scratched = False
         turn_is_over = False
 
         # If this is the final round, play until you beat the leader or scratch ...            
-        if final_round:
-            print(player.name,"is taking their final turn.")
-            while not turn_is_over and not scratched:
-                if player.score + turn_score < leader.score and remaining_dice > 0:
-                    dice = DiceSet()
-                    dice.roll(remaining_dice)
-                    print(player.name,"rolled",dice.values)
-                    if dice.score > 0:
-                        turn_score += dice.score
-                        remaining_dice = dice.remaining
-                    else:
-                        scratched=True
-                        print(player.name,"scratched and will score 0 points.")
-                else:
-                    turn_is_over = True
-                    player.score += turn_score
-                    print(player.name,"ends with",remaining_dice,"dice remaining and scored",turn_score,"points.")
-                    print(player.name,"now has", player.score,"points.")
-            
+        if game.final_round:
+            print(player.name,"is taking their final turn and currently has",player.score,"points.")
+            while player.score + turn_score < game.leader.score \
+                  and remaining_dice > 0 \
+                  and not player.scratched:
+                player, remaining_dice, turn_score = take_turn(player,remaining_dice,turn_score)
+             
+            if not player.scratched:
+                player.score += turn_score
+                print(player.name,"ends with",remaining_dice,"dice remaining and scored",turn_score,"points.")
+                print(player.name,"now has", player.score,"points.")
+                if player.score > game.leader.score:
+                    game.leader = player
+                    print(game.leader.name,"is the new leader.")
+
+            else:
+                print(player.name,"scratched and lost",turn_score,"points.")
+
         # Does this player still need to get on the board?
         elif player.score < 300:
             print(player.name,"is trying to get on the board.")
             
-            while remaining_dice > 0 and turn_score < 300:
-                dice = DiceSet()
-                dice.roll(remaining_dice)
-                print(player.name,"rolled", dice.values)
-                turn_score += dice.score
-                remaining_dice = dice.remaining
-                
+            # keep rolling until we have > 300 
+            while remaining_dice > 0 and turn_score < 300 and not player.scratched:
+                player, remaining_dice, turn_score = take_turn(player,remaining_dice,turn_score)
+                   
             if turn_score >= 300:
                 player.score = turn_score
                 print(player.name,"got on the board with", player.score,"points")
@@ -193,33 +239,19 @@ while not game.over:
                 print(player.name,"failed to get on the board with", turn_score,"points")
 
         else:
-            while not turn_is_over and not scratched:
-                if remaining_dice >=3:
-                    dice = DiceSet()
-                    dice.roll(remaining_dice)
-                    print(player.name,"rolled",dice.values)
-                    if dice.score > 0:
-                        turn_score += dice.score
-                        remaining_dice = dice.remaining
-                    else:
-                        scratched=True
-                        print(player.name,"scratched and will score 0 points.")
-                else:
-                    turn_is_over = True
-                    player.score += turn_score
-                    print(player.name,"will stop with",remaining_dice,"dice remaining and scored",turn_score,"points.")
-                    print(player.name,"now has", player.score,"points.")
-                    
-    if final_round: 
-        game.over = True
-        final_round_leader = game.leader
-        if final_round_leader.score > leader.score:
-            leader = final_round_leader
-    else:
-        leader = game.leader
-        print("End of round",game.rounds,":",leader.name,"is now the leader with",leader.score,"points.")
+            print(player.name, "is taking a turn and currently has",player.score,"points.")
 
-print("******************************************************")
-print("After",game.rounds,"rounds,",leader.name,"is the winner with",leader.score,"points.")
-            
-        
+            # Regular turn but stop when one die remains.
+            while remaining_dice > 1 and not player.scratched:
+                player, remaining_dice, turn_score = take_turn(player,remaining_dice,turn_score)
+             
+            if not player.scratched:
+                player.score += turn_score
+                print(player.name,"will stop with",remaining_dice,"dice remaining and scored",turn_score,"points.")
+                print(player.name,"now has", player.score,"points.")
+            else:
+                print(player.name,"scratched and will score 0 points.")
+                    
+    game.next_round()
+
+game.end()
